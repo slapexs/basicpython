cbe5dea2ccab996af703edb34d546a2264872bb7
onze <slapexs@gmail.com>
2022-05-24 22:01:27 +0700

    For loop

---
+++ for.py
@@ -0,0 +1,16 @@
+'''
+    For loop
+    for i in range(start, stop, step):
+        statement
+'''
+
+for i in range(4): # end
+    print("Round", i)
+print("----------")
+
+for i in range(1, 10, 2): # start stop end
+    print(i)
+print("----------")
+
+for i in range(1,4): # start, end
+    print(i)
