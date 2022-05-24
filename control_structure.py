d856a43ad813a88e12cc208e8ea86a7eba1d6b72
onze <slapexs@gmail.com>
2022-05-24 20:47:09 +0700

    Control structure if/else

---
+++ control_structure.py
@@ -0,0 +1,44 @@
+'''
+    โครงสร้างควบคุม Control structure
+    1. แบบลำดับ
+        if expresstion:
+            statement
+
+    
+    2. แบบเลือก
+        if expression:
+            statement
+        else:
+            statement
+
+        
+    3. แบบทำซ้ำ
+'''
+
+age = 55
+height = 32
+score = 80
+
+if age > 18:
+    print("Teen")
+
+if height > 60:
+    print("High")
+else:
+    print("Low")
+
+if score >= 80:
+    print("A")
+elif score >= 70:
+    print("B")
+elif score >= 60:
+    print("C")
+elif score >= 50:
+    print("D")
+else:
+    print("F")
+
+if 4>5 or 4<2:
+    print(True)
+else:
+    print(False)
