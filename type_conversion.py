4407dc181464d1c6abcc13d460413b848e750a8a
onze <slapexs@gmail.com>
2022-05-24 16:37:27 +0700

    Type conversion

---
+++ type_conversion.py
@@ -0,0 +1,17 @@
+# แปลงชนิดข้อมูล
+
+x = 10 #int
+y = 3.99 #float
+z = "20" #string
+t = "True"
+sum1 = x+y #int + float = float
+
+# Convert variable type
+int_z = int(z) # convert z to integer
+str_y = str(y) # convert y to string
+float_x = float(x) # convert x = float
+bool_t = bool(t) # Convert t = boolean
+
+name = "Yuna"
+int_name = int(name) # Error
+
