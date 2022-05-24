2b0a0dd8ac9d1641682d8ad10b3f8f596e3ee5e7
onze <slapexs@gmail.com>
2022-05-24 15:56:33 +0700

    Print function

---
+++ print.py
@@ -0,0 +1,19 @@
+# แสดงผลข้อมูลด้วยฟังก์ชัน
+'''
+    string -> ข้อความตัวอักษร
+    nunmber -> integer, float
+    boolean -> true, false
+'''
+
+print("Hello")
+print(1)
+print(3.99)
+print(True)
+print(False)
+print("Hello "+"world") # ต่อ String ด้วยเครื่องหมาน +
+print(1+9)
+print(4.000+1)
+
+# String 49 ต่อกับเลข 20 ที่แปลงเป็นstring
+print("49"+str(20))
+# output 4920
