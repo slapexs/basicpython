71d0e744fa9abb4549f705a1029e823f53e2bd9c
onze <slapexs@gmail.com>
2022-05-24 20:09:19 +0700

    Compound assignment

---
+++ compound_assignment.py
@@ -0,0 +1,19 @@
+'''
+    Compound assignment
+
+    ดำเนินการเกี่ยวกับตัวแปรทางด้านคณิตศาสตร์ เช่น
+    x = 5
+    x += 2 -> 7
+    x -= 2 -> 3
+    x *= 2 -> 10
+    x /= 5 -> 1
+    x %= 3 -> 2
+'''
+x = 10
+print("x = ", x)
+print("Before", x)
+x = x+5
+print("After", x)
+
+x -= 10
+print("Delete", x)
