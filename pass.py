dbb626ebac0e051a0065537a50910c40e5177669
onze <slapexs@gmail.com>
2022-05-24 20:57:14 +0700

    Pass for control structure

---
+++ pass.py
@@ -0,0 +1,13 @@
+# Control structure || Pass
+
+score = 65
+if score >= 80:
+    print("A")
+elif score >= 70:
+    print("B")
+elif score >= 60:
+    pass
+elif score >= 50:
+    pass
+else:
+    print("F")
