da83908f2488b108681a43a5935cb187f887f8d1
onze <slapexs@gmail.com>
2022-05-24 20:01:58 +0700

    logical

---
+++ logic.py
@@ -0,0 +1,20 @@
+''' 
+    ตัวดำเนินการทางตรรกศาสตร์
+    AND -> &&
+    OR -> ||
+    NOT -> !
+
+    ผลลัพธ์ที่ได้คือ True, False
+'''
+a = 5>4
+b = 4>7
+
+x = True
+y = False
+
+ex1 = x and y
+ex2 = x or y
+ex3 = not x
+ex4 = not y
+
+print(ex1)
