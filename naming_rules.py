3d90d90804992348f4bb10235eeb9e99ea43bfd9
onze <slapexs@gmail.com>
2022-05-24 16:22:28 +0700

    Naming rules

---
+++ naming_rules.py
@@ -0,0 +1,15 @@
+# กฎการตั้งชื่อ
+
+'''
+    1. ใส่ตัวเลข ตัวอักษร สัญลักษณ์
+    2. ห้ามขึ้นต้นด้วยตัวเลข และสัญลักณ์ยกเว้น _ (under scroll)
+    3. ห้ามใช้คำสงวน เช่น for if class while
+    4. Case sensitive ตัวพิมเล็ก!=ตัวพิมพ์ใหญ่ เช่น name != Name
+'''
+name = "Jenny"
+Name = "Rose"
+person1 = {"name":"Jim", "age":20}
+user_number1 = "user001"
+
+print(name)
+
