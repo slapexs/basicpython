308198d1bdd14a2275ec8db0a3c808ce98d7e220
onze <slapexs@gmail.com>
2022-05-24 15:45:32 +0700

    Comment

---
+++ comment.py
@@ -0,0 +1,9 @@
+# ใช้ปิดการทำงานของคำสั่ง
+# 1 line
+'''
+    Triple qoute for comment multiple lines
+    like that
+    bro!
+'''
+# print("Hello comment")
+print("Hello comment") #comment หลังจากการทำงาน
