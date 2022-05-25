044a0b1d560cd42c8bc597043b962f93f0de67cf
onze <slapexs@gmail.com>
2022-05-25 09:40:58 +0700

    zip for list

--- etc_method.py
+++ etc_method.py
@@ -2,6 +2,7 @@
     การเรียงข้อมูล
     .sort() -> น้อยไปมาก
     .reverse() -> ย้อนกลับ
+    list = list[::-1] -> เรียงลำดับใหม่จากหลังมาหน้าใน List เดิม
 '''
 
 numbers = [23,1,5,76,82,7,90,23,2,44,57]
@@ -10,6 +11,8 @@ numbers.sort()
 print(numbers) # [1, 2, 5, 7, 23, 23, 44, 57, 76, 82, 90]
 numbers.reverse()
 print(numbers) # [90, 82, 76, 57, 44, 23, 23, 7, 5, 2, 1]
+numbers = numbers[::-1]
+print(numbers) # [1, 2, 5, 7, 23, 23, 44, 57, 76, 82, 90]
 
 '''
     หาค่ามากสุด น้อยสุด
@@ -27,3 +30,16 @@ print(max(numbers)) # 90
 '''
 print(numbers) # [23,1,5,76,82,7,90,23,2,44,57]
 print(sum(numbers)) # 410
+
+
+'''
+    จับคู่คข้อมูลจาก 2 List
+    for x,y,z in zip(list1, list2, list3):
+        statement
+'''
+fruits = ["Apple", "Banana", "Mango"]
+prices = [10,20,30]
+weight = [1,2,3]
+
+for x,y,z in zip(fruits, prices, weight):
+    print(x,"->",y, "->", z)
